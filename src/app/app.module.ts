197327a7d3733f15219630469fce4c7282fd1582
ahmedmagdymohamedy <ahmedmagdymohamedy@gmail.com>
2022-10-06 05:18:41 +0200

    add local json service & ServerService for HttpClint requsts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatToolbarModule} from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
---
+++ src/app/services/server.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServerService {
+
+  static readonly BASE_URL: string = "http://localhost:3000";
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<any> {
+    return this.httpClient.get(ServerService.BASE_URL + "/products");
+  }
+
+  getProductByID(productID: number): Observable<any> {
+    return this.httpClient.get(ServerService.BASE_URL + '/product/' + productID);
+  }
+}
