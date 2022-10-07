da7b138f1adcc33cd1d632707d66f0f55472c201
ahmedmagdymohamedy <ahmedmagdymohamedy@gmail.com>
2022-10-07 22:33:45 +0200

    add product card

--- src/app/modules/product-details/product-details.module.ts
+++ src/app/modules/product-details/product-details.module.ts
@@ -2,17 +2,18 @@ import { AppModule } from './../../app.module';
 import { NgModule, OnInit } from '@angular/core';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CommonModule } from '@angular/common';
-import { StarRateComponent } from 'src/app/components/star-rate/star-rate.component';
+import { StarRateComponent } from 'src/app/shared/components/star-rate/star-rate.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     ProductDetailsComponent,
-    StarRateComponent,
   ],
   imports: [
     CommonModule,
+    SharedModule,
   ]
 })
 export class ProductDetailsModule {
