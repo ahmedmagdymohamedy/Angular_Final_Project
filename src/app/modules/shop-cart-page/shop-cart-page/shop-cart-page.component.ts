e6f26d8c64501bff2c635dbaaf413551c0cabd08
Ahmed Diboo <ah.diboo@gmail.com>
2022-10-11 03:54:11 +0200

    Update shop-cart-page.component.ts

--- src/app/modules/shop-cart-page/shop-cart-page/shop-cart-page.component.ts
+++ src/app/modules/shop-cart-page/shop-cart-page/shop-cart-page.component.ts
@@ -9,74 +9,73 @@ import { ServerService } from 'src/app/services/server.service';
 })
 export class ShopCartPageComponent implements OnInit {
 
-  constructor(private server:ServerService,private router:Router) {
-    
+  constructor(private server: ServerService, private router: Router) {
+
   }
-  carItemList:any[]=[];
-  total:number=0;
-  CartQuantityNumber:number=1;
-  
-  plusValue(order:any){
-    for(let item of this.carItemList){
-      if(item.id == order.id ){
+  carItemList: any[] = [];
+  total: number = 0;
+  CartQuantityNumber: number = 1;
+
+  plusValue(order: any) {
+    for (let item of this.carItemList) {
+      if (item.id == order.id) {
         item.product.count = item.product.count + 1
         this.calcTotal()
         console.log(item.product.count);
       }
     }
   };
-  
-  minusValue(order:any){
-    if(this.CartQuantityNumber=1){
 
-    }
-    for (let item of this.carItemList){
-      if(item.id == order.id ){
-        if(item.product.count>1){
-          item.product.count = item.product.count - 1
-          this.total= this.total - item.product.product.price
-          console.log(item.product.count);
+  minusValue(order: any) {
+    if (this.CartQuantityNumber = 1) {
+      for (let item of this.carItemList) {
+        if (item.id == order.id) {
+          if (item.product.count > 1) {
+            item.product.count = item.product.count - 1
+            this.total = this.total - item.product.product.price
+            console.log(item.product.count);
+          }
+
         }
-        
       }
-  }
+    }
+
   }
 
-  goToProductsScreen(){
+  goToProductsScreen() {
     this.router.navigate(['products']);
   }
-  goToPaymentScreen(){
+  goToPaymentScreen() {
     this.router.navigate(['payment']);
   }
-  regetOrder(){
-    this.server.getOrdersBYUserId(3).subscribe((data)=>{
+  regetOrder() {
+    this.server.getOrdersBYUserId(3).subscribe((data) => {
       this.carItemList = data
-      this.total=0
-      this.calcTotal() 
-    })  
+      this.total = 0
+      this.calcTotal()
+    })
   }
 
 
-  ngOnInit(): void 
-  {
-   this.regetOrder()
+  ngOnInit(): void {
+    this.regetOrder()
 
   }
 
 
 
-calcTotal(){    
-    for (let item of this.carItemList){
-    this.total= this.total + item.product.product.price
+  calcTotal() {
+    for (let item of this.carItemList) {
+      this.total = this.total + item.product.product.price
     }
-}
+  }
 
 
-deleteItem(order:any){
- this.server.deleteOrder(order.id).subscribe((data) =>{
-  this.regetOrder()
- },)
+  deleteItem(order: any) {
+    this.server.deleteOrder(order.id).subscribe((data) => {
+      this.regetOrder()
+    },)
 
 
-}
+  }
 }
